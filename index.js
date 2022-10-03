5436ea5300501aaec696341178f66c800b9a473d
PeresMiguel <msouza@realtec.com.br>
2022-10-03 16:05:51 -0300

    My resolution of the day 03/10

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function findBestSpot(landWidth, landHeight, exploitationWidth, exploitationHeight, goldMines) {
+module.exports = function findBestSpot(landWidth, landHeight, exploitationWidth, exploitationHeight, goldMines) {
+  // Generate the land
+  let land = (!landWidth || !landHeight) ? [[false]] : new Array(landWidth).fill(false).map(() => new Array(landHeight).fill(false))
+  //Explain mines
+  goldMines.map(mines => land[mines.x][mines.y] = true);
+
+  const exploitationMap = [];
+  const bestPoints = [];
+
+  const exploitationLimitWidth = (exploitationWidth > landWidth) ? landWidth : exploitationWidth
+  const exploitationLimitHeight = (exploitationHeight > landHeight) ? landHeight : exploitationHeight
+  // Verify neighbors
+  for (let x = 0; x < landWidth - exploitationLimitWidth + 1; x++) {
+    for (let y = 0; y < landHeight - exploitationLimitHeight + 1; y++) {
+      let amount = 0;
+      // Check limits and sum
+      for (let k = 0; k < exploitationLimitWidth; k++) {
+        for (let l = 0; l < exploitationLimitHeight; l++) {
+          if (land[x + k][y + l] !== false) {
+            amount += 1;
+          }
+        }
+      }
+      exploitationMap.push({x, y, amount});
+    }
+  }
+  //Get the most out of exploration
+  const maximumMine = Math.max.apply(Math, exploitationMap.map((o) =>  o.amount));
+  //Get the best points
+  for (const location of exploitationMap)
+    (location.amount === maximumMine) && bestPoints.push(location);
+
   return {
     coordinates: {
-      x: 0,
-      y: 0,
+      x: bestPoints[0].x,
+      y: bestPoints[0].y,
     },
-    goldMines : 0
+    goldMines: bestPoints[0].amount
   }
 }
 
-module.exports = findBestSpot;
+
